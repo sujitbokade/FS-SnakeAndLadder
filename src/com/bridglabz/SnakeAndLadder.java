2f585ce952c61596564e293fe24aa77eec7eab51
sujit <sujitbokade2014@gmail.com>
2022-07-08 17:17:32 +0530

    Roll the Dice

--- src/com/bridglabz/SnakeAndLadder.java
+++ src/com/bridglabz/SnakeAndLadder.java
@@ -1,9 +1,10 @@
 package com.bridglabz;
 
 public class SnakeAndLadder {
-    static final int START_POSITION=0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game !");
+        int Position=0;
         int no=(int)(Math.random()*10)%6+1;
         System.out.println(no);
     }
