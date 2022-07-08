a5b43b3aa18493133a3b730c0d1743f5a340e1bb
sujit <sujitbokade2014@gmail.com>
2022-07-08 17:02:05 +0530

    Option Check

--- src/com/bridglabz/SnakeAndLadder.java
+++ src/com/bridglabz/SnakeAndLadder.java
@@ -1,10 +1,29 @@
 package com.bridglabz;
 
 public class SnakeAndLadder {
-    static final int START_POSITION=0;
+    static final int LADDER=1;
+    static final int SNAKE=2;
+    static final int NO_PLAY=0;
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game !");
-        int no=(int)(Math.random()*10)%6+1;
-        System.out.println(no);
+        int Position=0;
+        int dice=(int)(Math.random()*10)%6+1;
+        System.out.println("dice "+dice);
+        int OptionCheck=(int)(Math.random()*10)%3;
+        System.out.println("Option "+OptionCheck);
+
+        if(OptionCheck==LADDER){
+            System.out.println("Ladder");
+            Position+=dice;
+        }
+        else if (OptionCheck==SNAKE ) {
+            System.out.println("Snake");
+            Position-=dice;
+        }
+        else {
+            System.out.println("No Play");
+            Position=Position;
+        }
+        System.out.println("Position "+Position);
     }
 }
