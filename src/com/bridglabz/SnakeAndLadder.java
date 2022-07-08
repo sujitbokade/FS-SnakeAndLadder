e2f4761eff224c59c583e42b72fb17878960b039
sujit <sujitbokade2014@gmail.com>
2022-07-08 16:16:35 +0530

    update code

--- src/com/bridglabz/SnakeAndLadder.java
+++ src/com/bridglabz/SnakeAndLadder.java
@@ -5,5 +5,6 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game !");
         int no=(int)(Math.random()*10)%6+1;
+        System.out.println(no);
     }
 }
